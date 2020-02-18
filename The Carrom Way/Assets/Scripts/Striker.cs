d302643e88163c363c76c06cf750982cb2298a44
Mahraf Ahmed <P1715160X@my365.dmu.ac.uk>
2020-02-18 05:25:06 +0000

    Fix script
    Fix the script & add comments while adding a background to the game.

--- The Carrom Way/Assets/Scripts/Striker.cs
+++ The Carrom Way/Assets/Scripts/Striker.cs
@@ -10,33 +10,41 @@ public class Striker : MonoBehaviour
 
     public GameObject ArrowDirection;
     Transform arrowTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        arrowTransform = ArrowDirection.transform;
+        rb2d = GetComponent<Rigidbody2D>(); // Declare Rigidbody2D
+        arrowTransform = ArrowDirection.transform; // Declare arrowTransformation
     }
 
     // Update is called once per frame
     void Update()
     {
-        arrowTransform.LookAt(Input.mousePosition, arrowTransform.up);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            print("S has been pressed!");
-            rb2d.AddForce(transform.up * strikeSpeed); // Transform Up * Speed of Striker = Movement; Just to see if moving the striker works.
+            Shoot(); 
         }
 
         if (rb2d.velocity.magnitude < 0.2f) // Set arrow when magnitude is less than 0.2 in value.
         {
-            ArrowDirection.SetActive(true);
+            ArrowDirection.SetActive(true); // Sets the arrow
         }
         else
         {
-            ArrowDirection.SetActive(false);
+            ArrowDirection.SetActive(false); // Disables the arrow
         }
     }
+
+    // Shoot is called when an input is selected
+    public void Shoot()
+    {
+        Vector3 worldToMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Finds the mouse position on the world
+        Vector2 dir = (Vector2)((worldToMousePos - transform.position)); // sets direction by calculating the difference between the location of mouse to position of striker
+        dir.Normalize(); // vector keeps the same direction but its length is 1.0.
+        rb2d.AddForce(dir * strikeSpeed); // Direction * Speed of Striker = Movement; Just to see if moving the striker works.
+    }
+
 }
 
 // Notes: For talcum powder I can set the linear drag and increase it.
